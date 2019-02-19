f983a3ad1d558a571ab37c88d2881b4c671fa8c3
yuchehseih <hsiehdanny860605@gmail.com>
2019-02-19 17:41:01 +0800

    [微重要] 在 index.html 引入 google font, 並完成主畫面的動畫！

--- src/components/home/featured/index.js
+++ src/components/home/featured/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Stripes from './stripes';
+import Text from './text';
 
 const Featured = () => {
   return (
     <div className="featured_wrapper">
       <Stripes />
+      <Text />
     </div>
   );
 };
