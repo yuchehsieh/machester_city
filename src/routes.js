2491aa876c72f80a317affe35b08486e8722674f
yuchehseih <hsiehdanny860605@gmail.com>
2019-02-19 11:22:43 +0800

    [重要] 重要的地方在 react-router-dom 的基本用法，有 Switch, Route 要引入

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import Layout from './hoc/layout';
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './components/home';
 
 const Routes = props => {
-  return <Layout>Second Line</Layout>;
+  return (
+    <Layout>
+      <Switch>
+        <Route exact component={Home} path="/" />
+      </Switch>
+    </Layout>
+  );
 };
 
 export default Routes;
