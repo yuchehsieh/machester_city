4cafb107fe01b26ebd8d95bf12e22b1c8ec9f10b
yuchehseih <hsiehdanny860605@gmail.com>
2019-02-28 16:09:41 +0800

    [重要] 加入 firebase 使用 firebase SDK 並且測試 database 的讀取功能

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/app';
+import 'firebase/database';
+
+const config = {
+  apiKey: 'AIzaSyBkm9NAumILNA2V_W0BbSFfepWkOKMTmy0',
+  authDomain: 'm-city-4be0a.firebaseapp.com',
+  databaseURL: 'https://m-city-4be0a.firebaseio.com',
+  projectId: 'm-city-4be0a',
+  storageBucket: 'm-city-4be0a.appspot.com',
+  messagingSenderId: '276189216264'
+};
+
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+
+firebaseDB
+  .ref('matches')
+  .once('value')
+  .then(snapshot => {
+    console.log(snapshot.val());
+  });
