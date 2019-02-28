c5f8f9b6ccdfc25c5a5e1656acb7eb28ea540604
yuchehseih <hsiehdanny860605@gmail.com>
2019-02-28 22:59:20 +0800

    把讀取到的資料呈現在畫面上，[重要的部分] CSS 撰寫、react-reveal 複習

--- src/components/home/matches/blocks.js
+++ src/components/home/matches/blocks.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { firebaseMatches } from '../../../firebase';
 import { firebaseLooper, reverseArray } from '../../ui/misc';
 
+import MatchesBlock from '../../ui/matches_block';
+import Slide from 'react-reveal/Slide';
+
 class Blocks extends Component {
   constructor(props) {
     super(props);
@@ -21,7 +24,18 @@ class Blocks extends Component {
       });
   }
 
-  showMatches = () => <div>match</div>;
+  showMatches = matches =>
+    matches
+      ? matches.map(match => (
+          <Slide bottom key={match.id}>
+            <div className="item">
+              <div className="wrapper">
+                <MatchesBlock match={match} />
+              </div>
+            </div>
+          </Slide>
+        ))
+      : null;
 
   render() {
     return (
