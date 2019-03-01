bcafeca0ec2de7df408a9931824fc72094fa3a70
yuchehseih <hsiehdanny860605@gmail.com>
2019-03-01 14:15:52 +0800

    製作 reusable 的 player card，[可參考] CSS 寫法！！！

--- src/components/home/meetPlayers/cards.js
+++ src/components/home/meetPlayers/cards.js
@@ -3,6 +3,8 @@ import { easePolyOut } from 'd3-ease';
 import Animate from 'react-move/Animate';
 import Otamendi from '../../../Resources/images/players/Otamendi.png';
 
+import PlayerCard from '../../ui/playerCard';
+
 class HomeCards extends Component {
   constructor(props) {
     super(props);
@@ -52,7 +54,12 @@ class HomeCards extends Component {
               bottom
             }}
           >
-            div
+            <PlayerCard
+              number="30"
+              name="Nicolas"
+              lastname="Otamendi"
+              bck={Otamendi}
+            />
           </div>
         )}
       </Animate>
