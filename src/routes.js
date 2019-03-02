c7a9ae2c1ba6fbd3fdc898bbaac7ca7a6a0946f2
yuchehseih <hsiehdanny860605@gmail.com>
2019-03-02 23:12:17 +0800

    [極重要] private route 的其中一種作法，幾個容易不懂的地方：解構、解構後重新命名、Route 的 component、spread operator 配合 props 的使用

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,25 @@ import React from 'react';
 import Layout from './hoc/layout';
 import { Switch, Route } from 'react-router-dom';
 
+import PrivateRoutes from './components/authRoutes/privateRoutes';
+
 import Home from './components/home';
 import SignIn from './components/signin';
 
 import Dashboard from './components/admin/dashboard';
 
 const Routes = props => {
+  console.log(props);
   return (
     <Layout>
       <Switch>
-        <Route exact component={Dashboard} path="/dashboard" />
+        <PrivateRoutes
+          {...props}
+          path="/dashboard"
+          exact
+          component={Dashboard}
+        />
+
         <Route exact component={SignIn} path="/sign_in" />
         <Route exact component={Home} path="/" />
       </Switch>
