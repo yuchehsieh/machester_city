022d4210d8fbaa85a77947f40fb49a7946cf423f
yuchehseih <hsiehdanny860605@gmail.com>
2019-03-02 21:19:16 +0800

    [重要可參考] 新增 dashboard Route 以及 AdminLayout 可以參考 AdminLayout 的作法跟 firebase 的登出

---
+++ src/components/admin/dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import AdminLayout from '../../hoc/adminLayout';
+
+const Dashboard = () => {
+  return (
+    <AdminLayout>
+      <div className="user_dashboard">
+        <div>This is your dashboard.</div>
+      </div>
+    </AdminLayout>
+  );
+};
+
+export default Dashboard;
--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,13 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/home';
 import SignIn from './components/signin';
 
+import Dashboard from './components/admin/dashboard';
+
 const Routes = props => {
   return (
     <Layout>
       <Switch>
+        <Route exact component={Dashboard} path="/dashboard" />
         <Route exact component={SignIn} path="/sign_in" />
         <Route exact component={Home} path="/" />
       </Switch>
